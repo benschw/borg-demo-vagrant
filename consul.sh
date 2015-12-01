246193109e4f90d7122d19a4e92d37ff1dca02f5
Ben Schwartz <benschw@gmail.com>
2015-12-01 16:40:22 -0600

    updating consul config to add repeater

--- consul.sh
+++ consul.sh
@@ -29,9 +29,46 @@ method: POST
 consumerstrategy: topic
 queueconfig:
   prefetch: 5
-  topicname: "#"
+  topicname: "borg-demo.default.#"
   queuename: borg
   exchangename: borg
+EOF
+
+read -r -d '' DC2_CFG << EOF
+name: dc2
+user: guest
+password: guest
+vhost: /
+host: localhost
+port: 5672
+EOF
+
+read -r -d '' REP_CFG << EOF
+name: Repeater
+servicename: foo
+uri: /foo
+method: POST
+consumerstrategy: topic
+deliverystrategy: topic-repeater
+queueconfig:
+  prefetch: 5
+  topicname: "borg-demo.dc2.#"
+  queuename: repeater-queue
+  exchangename: borg
+  connection: dc2
+  exchangeout: borg-dc2
+EOF
+read -r -d '' BORG_DC2_CFG << EOF
+name: BorgDC2
+servicename: borg-demo
+uri: /borg
+method: POST
+consumerstrategy: topic
+queueconfig:
+  prefetch: 5
+  topicname: "borg-demo.#"
+  queuename: borg-dc2
+  exchangename: borg-dc2
 EOF
 
 read -r -d '' RABBIT_SVC << EOF
@@ -47,6 +84,10 @@ echo "Configuring Consul"
 
 curl -X PUT http://172.20.20.10:8500/v1/kv/chinchilla/connection.yaml -d "$CONN_CFG"
 curl -X PUT http://172.20.20.10:8500/v1/kv/chinchilla/endpoints/borg.yaml -d "$BORG_CFG"
+
+curl -X PUT http://172.20.20.10:8500/v1/kv/chinchilla/repeater/connections/dc2.yaml -d "$DC2_CFG"
+curl -X PUT http://172.20.20.10:8500/v1/kv/chinchilla/endpoints/repeater.yaml -d "$REP_CFG"
+curl -X PUT http://172.20.20.10:8500/v1/kv/chinchilla/endpoints/repeated.yaml -d "$BORG_DC2_CFG"
 echo
 
 
